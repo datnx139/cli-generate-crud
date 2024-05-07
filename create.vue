4394e4ae3308f3dd94cba8e94572347b71cacc53
datnx139 <dat.nguyen@tomosia.com>
2024-05-07 18:24:09 +0700

    first commit

---
+++ create.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div>
+        <!-- Form để tạo mới /home/datnguyen/code/node-crud-cli/cli.js -->
+    </div>
+</template>
+<script>
+    // Logic để tạo mới /home/datnguyen/code/node-crud-cli/cli.js
+</script>
+    
---
+++ pages/tests/[id].vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div>
+        <!-- Form để tạo mới John -->
+    </div>
+</template>
+<script>
+    // Logic để tạo mới John
+</script>
+  
